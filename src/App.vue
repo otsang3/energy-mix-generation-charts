5c3a55ecdf1ab99f42651e552b9d0d5b1e6d6ab4
Oscar Tsang <ohhtsang@gmail.com>
2020-04-23 12:48:38 +0100

    adds fetch data

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,27 @@
 <template lang="html">
-
+ <h1>Enery Mix UK</h1>
 </template>
 
 <script>
 export default {
+  name: 'app',
+  data(){
+    return {
+      chartData: [],
+      timeFrom: "",
+      timeTo:""
+    }
+  },
+  mounted(){
+    fetch('https://api.carbonintensity.org.uk/generation')
+    .then(res => res.json())
+    .then(data => {
+      this.chartData = data.data.generationmix;
+      this.timeFrom = data.data.from;
+      this.timeTo = data.data.to;
+    })
+  }
+
 }
 </script>
 
